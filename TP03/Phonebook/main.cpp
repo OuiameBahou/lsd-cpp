234e45e8577acadbd5e7619a026a988994027895
OuiameBahou <92057724+OuiameBahou@users.noreply.github.com>
2023-01-09 16:43:42 +0100

    Update main.cpp

--- TP03/Phonebook/main.cpp
+++ TP03/Phonebook/main.cpp
@@ -2,16 +2,16 @@
 #include<string>
 #include "phonebook.hpp"
 using namespace std;
-string makeTen(string a){
-    int n=a.length();
+string Ten(string s){
+    int n=s.length();
     string res="";
     if(n==10){
     }
     if(n>10){
-        res=a.substr(0,10);
+        res=s.substr(0,10);
     }
     if(n<10){
-        res=a;
+        res=s;
         for (int i=n; i<10;i++){
             res+=" ";}
     }
@@ -22,18 +22,18 @@ string makeTen(string a){
 
 void PhoneBook::ADD(){
 
-    Contact c;
-    c.getdata();
+    Contact cont;
+    cont.getinput();
     cout<<"contact created !";
     if(index==8)
     {
-        contarr[0]=c;
+        contarr[0]=cont;
         return;
         
     }
     else
     {
-        contarr[index]=c;
+        contarr[index]=cont;
         index++;
         return;
     }
@@ -43,7 +43,7 @@ void PhoneBook::SEARCH()
     for(int i=0;i<index;i++)
     {
         cout<<i<<"|"<<endl;
-        contarr[i].showdata();
+        contarr[i].showinput();
     }
     return;
     
@@ -54,7 +54,7 @@ void PhoneBook::EXIT()
     
     
 }
-void Contact::getdata(){
+void Contact::getinput(){
     cout<<"give a first_name :";
     cin>>first_name;
     cout<<"give a last_name : ";
@@ -65,10 +65,10 @@ void Contact::getdata(){
     cin>>phonenumber;
 }
 
-void Contact::showdata(){
-    cout<<makeTen(first_name)<<"|";
-    cout<<makeTen(last_name)<<"|";
-    cout<<makeTen(nickname)<<"";
+void Contact::showinput(){
+    cout<<Ten(first_name)<<"|";
+    cout<<Ten(last_name)<<"|";
+    cout<<Ten(nickname)<<"";
 }
 int menu()
 {
@@ -83,17 +83,17 @@ int menu()
 }
 int main()
 {
-    PhoneBook PhoneBook1;
+    PhoneBook PhoneBookk;
     int option;
     do{
         option = menu();
         switch(option)
         {
-            case 1:PhoneBook1.ADD();
+            case 1:PhoneBookk.ADD();
                 break;
-            case 2:PhoneBook1.SEARCH();
+            case 2:PhoneBookk.SEARCH();
                 break;
-            case 3:PhoneBook1.EXIT();
+            case 3:PhoneBookk.EXIT();
                 break;
             default: cout<<"invalid option !\n\n";
                 break;
